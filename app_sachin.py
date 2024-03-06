9620b9f437f758d4ac26cf6406b9acd35b71b772
Sachin Goud <sachin_goud@Sachins-MacBook-Air.local>
2024-03-06 11:07:20 +0530

    some changes

--- app_sachin.py
+++ app_sachin.py
@@ -1,4 +1,5 @@
 import streamlit as st
+
 # import PyPDF
 import string
 import os
@@ -10,21 +11,29 @@ import openai
 
 
 load_dotenv()
-
 os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
 openai.api_key = os.getenv("OPENAI_API_KEY")
+st.set_page_config(layout="wide")
+st.title("Chat with your documents")
+
+st.write("key = ", openai.api_key)
+
 
 def semantic_search(query):
-    data = SimpleDirectoryReader('data').load_data()
+    data = SimpleDirectoryReader("data").load_data()
     index = GPTVectorStoreIndex.from_documents(data, show_progress=True)
     query_engine = index.as_query_engine()
     response = query_engine.query(query)
-    return(response)
+
+    return response
+
 
 def summarize():
-    data = SimpleDirectoryReader('data').load_data()
+    data = SimpleDirectoryReader("data").load_data()
     index = GPTListIndex.from_documents(data)
-    query_engine = index.as_query_engine(response_mode="tree_summarize",verbose=True, streaming = True )
+    query_engine = index.as_query_engine(
+        response_mode="tree_summarize", verbose=True, streaming=True
+    )
     response = query_engine.query("Summarize the document")
     return response
 
@@ -34,28 +43,27 @@ def save_uploaded_pdf(pdf, pdf_path):
         file.write(pdf.getbuffer())
     return st.success("Saved the {} pdf to directory".format(pdf.name))
 
+
 def displayPDF(file):
     # Opening file from file path
     with open(file, "rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+        base64_pdf = base64.b64encode(f.read()).decode("utf-8")
 
     # Embedding PDF in HTML
-    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
 
     # Displaying File
     st.markdown(pdf_display, unsafe_allow_html=True)
 
-st.set_page_config(layout='wide')
-st.title("Chat with your documents")
 
-pdf= st.file_uploader("Upload your PDF", type=['pdf'])
+pdf = st.file_uploader("Upload your PDF", type=["pdf"])
 
-# What is the total experience of the candidate?
+
 if pdf is not None:
-    col1, col2, col3 = st.columns([1,1,2])
+    col1, col2, col3 = st.columns([2, 1, 1])
 
     with col1:
-        pdf_path = "data/"+pdf.name
+        pdf_path = "data/" + pdf.name
         ip_file = save_uploaded_pdf(pdf, pdf_path)
         display_pdf = displayPDF(pdf_path)
 
@@ -63,11 +71,10 @@ if pdf is not None:
         st.success("Search Area")
         query_search = st.text_area("Search your query")
         if st.button("Search"):
-            st.info("Your query: "+query_search)
-            
+            st.info("Your query: " + query_search)
+
             result = semantic_search(query_search)
             st.text_area("Output", value=result, height=300)
-            # st.text(result)
 
     with col3:
         st.success("Document summary")
@@ -75,4 +82,3 @@ if pdf is not None:
             st.info("Generating the document summary...")
             summary_res = summarize()
             st.text_area("Output", value=summary_res, height=500)
-            # st.text(summary_res)
